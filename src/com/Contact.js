24a3a5c6b4e75e645e92292c600df03d109d0586
mziyasur <mehmetziyasurucu@outlook.com>
2021-05-04 04:26:07 +0300

    Mapview eklendi.

--- src/com/Contact.js
+++ src/com/Contact.js
@@ -6,11 +6,40 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
 
 function ContactScreen({ navigation }) {
+  const coordEv = {
+    latitude: 41.01466,
+    longitude: 28.629642,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
   return (
     <View style={{ flex: 1, alignItems: "center" }}>
-      <Text>İletişim Bilgileri</Text>
+      <Text style={styles.font}>İletişim Bilgileri</Text>
+      <Text style={styles.fontsub}>
+        KAAN TİCARET D.T.M. İTH. İHR. DAH. TİC.
+      </Text>
+      <Text style={styles.fontsub}>
+        Şehitler Mah. 2633. Sk. No:3/B Kıraç / Esenyurt / İSTANBUL
+      </Text>
+      <Text style={styles.fontsub}>GSM : +90 (532) 165 31 50</Text>
+      <Text style={styles.fontsub}>E-Posta : kaan@kromevye.com.tr</Text>
+      <Text style={styles.fontsub}>Web : www.kaanevye.com</Text>
+
+      <MapView style={styles.map} initialRegion={coordEv}>
+        <MapView.Marker
+          coordinate={{ latitude: 41.01466, longitude: 28.629642 }}
+          title={"EV"}
+          description={"Home"}
+        />
+      </MapView>
     </View>
   );
 }
@@ -26,3 +55,24 @@ function ContactStackScreen() {
 }
 
 export default ContactStackScreen;
+
+const styles = StyleSheet.create({
+  map: {
+    width: wp("100%"),
+    height: hp("45%"),
+  },
+  font: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  fontsub: {
+    fontSize: 20,
+    fontWeight: "bold",
+    paddingTop: 5,
+    paddingBottom: 0,
+    marginBottom: 5,
+    marginTop: 5,
+    marginRight: 20,
+    marginLeft: 20,
+  },
+});
